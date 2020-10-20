dec7164212fccb13897a1c47a671d531e24f842b
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2020-10-19 22:44:27 -0300

    first commit, project created

---
+++ src/main/java/com/example/sbmdb/SpringBootMongDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongDbApplication.class, args);
+	}
+
+}
