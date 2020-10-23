d928870b55cfb0dd01d978aceffaa9ada53ba2b7
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2020-10-22 22:11:00 -0300

    Creating Post entity with User nested

--- src/main/java/com/example/sbmdb/config/Instantiation.java
+++ src/main/java/com/example/sbmdb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.example.sbmdb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.sbmdb.domain.Post;
 import com.example.sbmdb.domain.User;
+import com.example.sbmdb.repository.PostRepository;
 import com.example.sbmdb.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,26 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null,sdf.parse("21/03/2020"),"partiu sp","viagem braba dms",maria);
+		Post post2 = new Post(null,sdf.parse("21/03/2020"),"bom dia!","Acordei feliz!",maria);
+		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
 }
