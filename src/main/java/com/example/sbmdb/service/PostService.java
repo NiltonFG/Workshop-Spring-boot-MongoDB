57e2e6e47be07052fce9c61514dffd3f885b692d
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2020-10-22 22:45:04 -0300

    Getting post by Id

---
+++ src/main/java/com/example/sbmdb/service/PostService.java
@@ -0,0 +1,22 @@
+package com.example.sbmdb.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.sbmdb.domain.Post;
+import com.example.sbmdb.repository.PostRepository;
+import com.example.sbmdb.service.exception.ObjectNotFoundExcepition;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository rep;
+
+	public Post findById(String id) {
+		Optional<Post> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExcepition("Objeto não encontrado"));
+	}
+}
