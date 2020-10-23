0fbd260954994d3dd3d1f2e3c6726966e2580681
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2020-10-22 22:38:09 -0300

    Endpoint posts

--- src/main/java/com/example/sbmdb/resources/UserResource.java
+++ src/main/java/com/example/sbmdb/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.sbmdb.domain.Post;
 import com.example.sbmdb.domain.User;
 import com.example.sbmdb.dto.UserDTO;
 import com.example.sbmdb.service.UserService;
@@ -60,4 +61,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
