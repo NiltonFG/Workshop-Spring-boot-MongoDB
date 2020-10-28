d54873cc0b1a544ef7d24fcbab2a64d458eff823
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2020-10-28 08:35:25 -0300

    Simple search with Query method

--- src/main/java/com/example/sbmdb/repository/PostRepository.java
+++ src/main/java/com/example/sbmdb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.sbmdb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.example.sbmdb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String title);
+	;
 }
